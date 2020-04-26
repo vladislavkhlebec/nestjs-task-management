ebb7e28e108bcf7e2ca6524a7fdc6dc6c84a211b
Vladislav <vladislavkhlebec@gmail.com>
2020-04-26 21:09:02 +0300

    feat: create crud operations

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common'
+import { Task, TaskStatus } from './task.model'
+import { v1 as uuidv1 } from 'uuid'
+import { CreateTaskDto } from './dto/create-task.dto'
+
+@Injectable()
+export class TasksService {
+	private tasks: Task[] = []
+
+	getAllTasks(): Task[] {
+		return this.tasks
+	}
+
+	getTaskById(id: string): Task {
+		return this.tasks.find(task => task.id === id)
+	}
+
+	createTask(createTaskDto: CreateTaskDto) {
+		const { title, description } = createTaskDto
+
+		const task: Task = {
+			id: uuidv1(),
+			title,
+			description,
+			status: TaskStatus.OPEN,
+		}
+
+		this.tasks.push(task)
+		return task
+	}
+
+	deleteTask(id: string): void {
+		this.tasks = this.tasks.filter(task => task.id !== id)
+	}
+
+	updateTaskStatus(id: string, status: TaskStatus) {
+		const task = this.getTaskById(id)
+		task.status = status
+		return task
+	}
+}
