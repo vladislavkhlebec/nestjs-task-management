b3f8bd0bfea7d8ef32d22c657977a65cc6c84480
Vladislav <vladislavkhlebec@gmail.com>
2020-04-27 18:45:22 +0300

    feat: add pipes and exeprtion

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,39 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
 import { TasksService } from './tasks.service'
 import { Task, TaskStatus } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 
 @Controller('/tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService) {}
 
 	@Get()
-	getAllTasks(): Task[] {
-		return this.tasksService.getAllTasks()
+	getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+		return Object.keys(filterDto).length
+			? this.tasksService.getTasksWithFilters(filterDto)
+			: this.tasksService.getAllTasks()
 	}
 
 	@Get('/:id')
 	getTaskById(@Param('id') id: string): Task {
-			return this.tasksService.getTaskById(id)
+		return this.tasksService.getTaskById(id)
 	}
 
 	@Post()
+	@UsePipes(ValidationPipe)
 	createTask(@Body() createTaskDto: CreateTaskDto): Task {
 		return this.tasksService.createTask(createTaskDto)
 	}
 
 	@Delete('/:id')
-	deleteTask(@Param('id') id:string ): void {
+	deleteTask(@Param('id') id: string): void {
 		this.tasksService.deleteTask(id)
 	}
 
 	@Patch('/:id/status')
-	updateTaskStatus(
-		@Param('id') id:string,
-		@Body('status') status: TaskStatus
-	): Task {
+	updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
 		return this.tasksService.updateTaskStatus(id, status)
 	}
 }
-
-
